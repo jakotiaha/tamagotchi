c34f86bde666df387655cda6082f6deb6887bc5b
Akshat Jakotiah <jakotiah.a@etoncollege.org.uk>
2023-01-25 09:42:31 +0000

    25/1 almost end of tamagotchi

--- Tamagotchi/ContentView.swift
+++ Tamagotchi/ContentView.swift
@@ -8,26 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tamagotchi = Tamagotchi(hunger: 5, awake: true, discipline: 5, age: 0, full: false, happiness: 5)
+    @State private var tamagotchi = Tamagotchi(hunger: 5, awake: true, age: 0, full: false, happiness: 5, droppings: false, health: 5,  bathroomNeeded: 5)
     
     @State private var full = false
+    @State private var timeTillDroppings = 20
+    @State private var droppings = false
     
     var fullIsTrue: Bool {
         return full ? false : tamagotchi.hunger > 0 ? false : true
     }
     
+    var maxHappiness: Bool {
+        return full ? true : tamagotchi.happiness > 10 ? true : false
+    }
+    
+    let timer = Timer.TimerPublisher(interval: 1, runLoop: .main, mode: .common)
+
+    
     var body: some View {
-       Form{
-        Text("\(tamagotchi.displayStats())")
-           VStack{
-               Button("Feed Tamagotchi meal", action:{ tamagotchi.feedmeal()})
-                   .disabled(fullIsTrue)
-           }
-           Button("Feed Tamagotchi snack", action:{ tamagotchi.feedsnack()})
+        VStack{
+           Form{
+               Text("TAMAGOTCHI")
+                   .foregroundColor(.orange)
+               Text("\(tamagotchi.displayStats())")
+               Text("Time till droppings: \(timeTillDroppings)")
+                   .onReceive(timer) { _ in
+                       if self.timeTillDroppings > 0 {
+                           self.timeTillDroppings -= 1
+                       } else {
+                           self.droppings = true
+                       }
+                   }
+               VStack{
+                   Button("Feed Tamagotchi meal", action:{ tamagotchi.feedmeal()})
+                       .disabled(fullIsTrue)
+                       
+               }
+               Button("Feed Tamagotchi snack", action:{ tamagotchi.feedsnack()})
                .disabled(fullIsTrue)
+               
+               Button("Play game", action:{ tamagotchi.playGame()})
+               .disabled(maxHappiness)
+               
+               Button("Grow old", action:{ tamagotchi.growOld()})
+               
+               Button("Get ill", action:{ tamagotchi.getIll()})
+               Button("Give medicine", action:{ tamagotchi.giveMedicine()})
+            
        }
      }
-    
+    }
 
 }
 
--- Tamagotchi/TamagotchiInfo.swift
+++ Tamagotchi/TamagotchiInfo.swift
@@ -10,24 +10,27 @@ import Foundation
 struct Tamagotchi {
     var hunger: Int
     var awake: Bool
-    var discipline: Int
     var age: Int
     var full: Bool
     var happiness: Int
+    var droppings: Bool
+    var health: Int
+    var bathroomNeeded: Int
     
     func displayStats()-> String {
         return """
-         Hunger: \(hunger)/5
+         Hunger: \(hunger)/10
          Awake: \(awake)
-         Discipline: \(discipline)/5
          Age: \(age)
-         Happiness:\(happiness)/5
+         Happiness:\(happiness)/10
+         Health: \(health)/10
+         Bathroom Needed: \(bathroomNeeded)/10
 
          """
     }
     
     mutating func feedmeal(){
-        if self.hunger != 0{
+        if self.hunger > 1{
             self.hunger -= 2
             }
         }
@@ -38,8 +41,50 @@ struct Tamagotchi {
             }
         }
     
-    func playGame() {
+    mutating func playGame(){
+        if self.happiness != 10{
+            self.happiness += 1
+        }
         
+     }
+    
+    mutating func growOld(){
+        if self.age != 25{
+            self.age += 1
+        }
+        
+    }
+        
+    mutating func bathroomAndClean(){
+            if self.bathroomNeeded != 10{
+                self.bathroomNeeded += 1
+                if self.bathroomNeeded == 10{
+                        self.droppings = true
+            
+                
+        }
+        
+     }
+    
+    }
+    
+    mutating func getIll(){
+        if self.health > 0{
+            self.health -= 1
+            }
+        if self.happiness > 0{
+            self.happiness -= 1
+        }
+    
+    }
+    
+    mutating func giveMedicine(){
+        if self.health < 10{
+            self.health += 1
+        }
+        if self.happiness < 10{
+            self.happiness += 1
+        }
     }
 
 }
